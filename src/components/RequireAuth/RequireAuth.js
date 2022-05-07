b5937c399522fbb9878778e34451b6037c722321
jankar-with-tarek <avirus170@gmail.com>
2022-05-07 10:47:53 +0600

    provlem solved

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -12,7 +12,7 @@ const RequireAuth = ({ children }) => {
         return <Spinner></Spinner>
     }
 
-    if (user.emailVerified && !user) {
+    if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
 
