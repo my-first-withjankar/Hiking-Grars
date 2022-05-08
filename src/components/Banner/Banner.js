73cc239b342ec2421fe38e4c766ae70f5804d0d5
jankar-with-tarek <avirus170@gmail.com>
2022-05-08 19:39:27 +0600

    finished

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -23,7 +23,7 @@ const Banner = () => {
                         style={{ fontWeight: 'bold', color: 'rgb(5, 51, 83)' }}
                         className='display-2'>Exploring
                     </h1>
-                    <p className='w-50 mt-4 text-black'>Stock the latest lightweight backpacking equipment and hiking gear from top brands like Western Mountaineering, MSR, Therm-a-Rest, Osprey, and Hilleberg to make your next backcountry trip easier and faster. Not quite looking to hike the PCT? Shop our huge selection of luxury car camping gear to outfit your crew with super-comfy sleeping pads, a 6-person tent, and a gourmet camp kitchen. No matter how you travel in the outdoors, we have the camping outdoor gear to get you there. There's no telling who might explore with you when you're equipped and leading the way..</p>
+                    <p className='w-50 mt-4 text-black'>Stock the latest lightweight backpacking equipment and hiking gear from top brands like Western Mountaineering, MSR, Therm-a-Rest, Osprey, and Hilleberg to make your next backcountry trip easier and faster. Not quite looking to hike the PCT? Shop our huge selection of luxury car camping gear to outfit </p>
                 </div>
             </div>
 
